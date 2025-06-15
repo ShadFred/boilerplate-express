eac5a18558074feac7e4be074d82118984d0bef2
ShadFred <gvkashin@gmail.com>
2025-06-16 02:23:29 +0300

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,6 +1,46 @@
-let express = require('express');
-let app = express();
-console.log("Hello World")
+var express = require('express');
+require('dotenv').config();
+var app = express(); 
+console.log(process.env.MESSAGE_STYLE);
+
+//let app = express();
+//console.log(process.env//.VARIABLE_ONE)
+
+
+//app.use('/pics', express.static(__dirname + '/images'));
+//console.log("Hello World");
+/** app.get("/", (req,res)=>{
+  res.send("Hello Express");
+
+});*/
+
+// app.get("/",function(req,res){
+  //res.sendFile(__dirname + "/views/index.html");
+//});
+
+
+
+//serve json on a specific route
+//app.get('/json',(request,response)=> {
+  //response.json(message);
+//});
+
+/**6)use the .env File to configure app*/
+
+app.get("/", function(req, res){
+  res.sendFile(__dirname+ '/views/index.html');
+app.use(express.static(__dirname + "/public"));
+})
+//7.use the .env file
+let message = { message: "Hello json" };
+app.get("/json", (request, response) => {
+  if (process.env.MESSAGE_STYLE === "uppercase") {
+    response.json({ message: "HELLO JSON" });
+  } else {
+    response.json(message);
+  }
+});
+module.exports = app;
 
 
 
@@ -30,10 +70,3 @@ console.log("Hello World")
 
 
 
-
-
-
-
-
-
- module.exports = app;
